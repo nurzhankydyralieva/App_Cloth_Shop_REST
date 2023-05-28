33475418348fcac6c416ac0bf1f6cf803d266537
Nurzhan <nurzhankydyralieva@gmail.com>
2023-05-28 20:57:29 +0600

    first commit

---
+++ src/main/java/com/epam/porject/ClothShopAppApplication.java
@@ -0,0 +1,13 @@
+package com.epam.porject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClothShopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClothShopAppApplication.class, args);
+	}
+
+}
